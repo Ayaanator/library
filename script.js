d9811be77801233afc333a655234566b9923f8e3
Ayaan Adrito <ayaan.adrito@gmail.com>
2025-07-23 16:13:44 -0400

    Initialize

---
+++ script.js
@@ -0,0 +1,70 @@
+const myLibrary = [];
+const book_adder = document.querySelector("#book-adder");
+const modal = document.querySelector("#modal");
+const submit_buttom = document.querySelector("#submit-button");
+const cancel_button = document.querySelector("#cancel-button");
+const book_form = document.querySelector("#book-form");
+
+book_adder.addEventListener("click", () => {
+        modal.showModal();
+    }
+)
+
+submit_buttom.addEventListener("click", () => {
+        addBookToLibrary();
+        book_form.reset();
+    }
+)
+
+cancel_button.addEventListener("click", () => {
+        modal.close();
+        book_form.reset();
+    }
+)
+
+function Book(title, author, pages, read, id) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = id;
+}
+
+const book1 = new Book("The Lightning Thief", "Rick Riordan", 315);
+const book2 = new Book("The Alchemyst", "Paulo Cohelo", 24);
+myLibrary.push(book1);
+myLibrary.push(book2);
+
+function generateRandomId(prefix = "id") {
+  return prefix + "_" + Math.random().toString(36).substr(2, 9);
+}
+
+function addBookToLibrary() {
+
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const read = document.getElementById("read").value;
+
+    let book = new Book(title, author, pages, read, generateRandomId("book"));
+    myLibrary.push(book);
+    printBooks();
+}
+
+function printBooks() {
+    myLibrary.forEach( book => {
+            console.log(book.title + " " + book.author + " " + book.pages + " " + book.read + " " + book.id);
+        }
+    )
+}
+
+function addBookCard(book) {
+    const div = document.createElement("div");
+    div.className = "card";
+
+    const title = document.createElement("h1");
+    title.textContent = book.title;
+    
+    const author = document.createElement("h2");
+    author.textContent = book.author;
+}
